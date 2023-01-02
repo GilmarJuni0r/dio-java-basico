17f8d8ef1957b64e00c93f73af451979bb204957
GilmarJuni0r <thejumare@gmail.com>
2023-01-02 18:15:59 -0300

    Testando tipos de variaveis

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5; 
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14;
+        //VALOR_DE_PI = 10.75; <-- Constante
+
+        System.out.println(numero);
     }
 }
